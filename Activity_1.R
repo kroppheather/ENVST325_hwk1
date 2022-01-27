a5a4913c0eb119ac534db325621cda7db44bb85e
kroppheather <kroppheather@gmail.com>
2022-01-27 16:33:22 +0000

    activity 1 examples

--- Activity_1.R
+++ Activity_1.R
@@ -1,2 +1,20 @@
 # Activity & homework 1
 
+6*6
+6/6
+5^5
+
+myVec <- c(3,45,63,5,2)
+
+myData <- data.frame(col1=c(4,3),
+                     col2=c(2,5))
+
+myVec*10
+
+######### In class prompt 1 ----
+# Prompt 1
+
+
+
+
+
